178b30d2c689295ce1179faf038ee0b40106869e
Frost1801 <sergio.cibecchini@gmail.com>
2023-02-27 13:55:43 +0100

    Web13

---
+++ Web/Web13/main.py
@@ -0,0 +1,17 @@
+import requests as rr
+from bs4 import BeautifulSoup
+
+url = "http://web-13.challs.olicyber.it/"
+
+result = "flag{"
+
+r = rr.get(url)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+highlighted = soup.find_all("span")
+
+for t in highlighted:
+    result+= t.text
+result += "}"
+print(result)
